978ad85185e59525bb28e71ea8c723c87ff93155
Sunil <127929985+mrsunil3600@users.noreply.github.com>
2026-03-27 23:04:33 +0530

    Update AuthJwtUtil.java

--- src/main/java/SercurityTest/TestSecurity/jwtservice/AuthJwtUtil.java
+++ src/main/java/SercurityTest/TestSecurity/jwtservice/AuthJwtUtil.java
@@ -16,29 +16,5 @@ import java.util.Date;
 public class AuthJwtUtil {
     @Value("${jwt.secret.key}")
     private  String secretkey;
-    private SecretKey secretKey(){
-        return Keys.hmacShaKeyFor(secretkey.getBytes(StandardCharsets.UTF_8));
-    }
-    public  String createToken(UserEntity user){
-    String token=Jwts.builder()
-            .setSubject(user.getUsername())
-            .claim("Id",user.getId())
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis()+(1000*60*60)))
-            .signWith(secretKey())
-            .compact();
-    return token;
-    }
-
-    public Claims extractToken(String token){
-        return  Jwts.parserBuilder()
-                .setSigningKey(secretKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    public String extractUsername(String token){
-        return  extractToken(token).getSubject();
-    }
+  
 }
